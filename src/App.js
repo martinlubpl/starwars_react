b8ff7d318df02d61c7fdcab0a16397b31f849afc
Marcin Majewski <martinpl@gmail.com>
2022-06-15 06:34:25 +0200

    setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      text: 'TEST2'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            asdf {this.state.text}
+          </p>
+          <button onClick={() => {
+            this.setState({ text: 'HELLO!' })
+          }} >change text</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
