777ae7adedae5b397a4dd3ab6b7de40e893c7efb
Marcin Majewski <martinpl@gmail.com>
2022-06-17 17:21:20 +0200

    cardList css

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,12 +1,23 @@
 import { Component } from "react";
+import './card-list.styles.css'
 
 class CardList extends Component {
     render() {
         const { characters } = this.props;
         return (
             <div className='card-list' >
-                {characters.map(character => {
-                    return <h1 key={character.id}>{character.name}</h1>
+                {characters.map((character) => {
+                    const { name, email, id } = character
+                    return (
+                        <div className="card-container" key={id}>
+                            <img alt={`character ${name}`} src={`images/character${id}.png`}></img>
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div>
+                    )
+
+
+                    // <h1 key={character.id}>{character.name}</h1>
                 })}
             </div>
         )
